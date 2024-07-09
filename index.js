c8c07aa8fe973aae30509d7b1ea4de00c59d0a4e
Purity Jepkorir <jepkorirpurity8@gmail.com>
2024-07-09 15:27:10 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {
+  name: "Purity Jepkorir",
+  streetAddress: "145 Tom Mboa Street",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  let newObj = { ...obj };
+  newObj[key] = value;
+  return newObj;
+}
+updateEmployeeWithKeyAndValue(employee, "sam", "11 Broadway");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+  let newObj = { ...obj };
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
